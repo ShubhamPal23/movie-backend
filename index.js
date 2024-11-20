ef9cc923d8ce26ee824433a83007ce5ca71fe15e
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-11-21 02:12:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import connectDB from "./db/connection.js";
 import cors from 'cors';
 const app = express();
 const port = 3000;
-app.use(cors());
+app.use(cors({
+    origin: 'https://glowing-swan-31eb1f.netlify.app/',
+}));
 app.use(express.json());
 app.use(express.static("public"));
 connectDB()
